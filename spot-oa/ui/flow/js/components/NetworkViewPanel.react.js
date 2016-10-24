28e21a485c32680224cbf34376c583552e7335f4
daortizh <diego.ortiz.huerta@intel.com>
2016-10-24 10:25:59 -0500

    Netflow and DNS: Performance improvement for Network View (#111)
    * Netflow: NetworkView panel refactor. Performance improvement and now using PolloMixin
    
    * DNS: NetworkView panel performance improved and now using PolloMixin
    
    * Documentation update

---
+++ spot-oa/ui/flow/js/components/NetworkViewPanel.react.js
@@ -0,0 +1,154 @@
+const React = require('react');
+
+const ContentLoaderMixin = require('../../../js/components/ContentLoaderMixin.react');
+const ChartMixin = require('../../../js/components/ChartMixin.react');
+const PolloNetworkViewMixin = require('../../../js/components/PolloNetworkViewMixin.react');
+
+const SpotActions = require('../../../js/actions/SpotActions');
+const EdInActions = require('../../../js/actions/EdInActions');
+const SpotConstants = require('../../../js/constants/SpotConstants');
+
+const SuspiciousStore = require('../stores/SuspiciousStore');
+
+const ID_SEPARATOR = '-';
+
+function createNodeId(id) {
+    return `node${ID_SEPARATOR}${id}`;
+}
+
+function getIpFromNodeId(id) {
+    return id.split(ID_SEPARATOR)[1];
+}
+
+function getLinkSource(sourceIp, destinationIp) {
+    return sourceIp<=destinationIp ? sourceIp : destinationIp;
+}
+
+function getLinkDestination(sourceIp, destinationIp) {
+    return sourceIp>destinationIp ? sourceIp : destinationIp;
+}
+
+function createLinkId(sourceIp, destinationIp) {
+    const srcId = getLinkSource(sourceIp, destinationIp);
+    const dstId = getLinkDestination(sourceIp, destinationIp);
+
+    return `link${ID_SEPARATOR}${srcId}${ID_SEPARATOR}${dstId}`;
+}
+
+function getNodesFromData(data) {
+    const nodes = {};
+    const mapper = {srcIP: 'srcIpInternal', dstIP: 'destIpInternal'};
+
+    data.forEach((item) => {
+        ['srcIP', 'dstIP'].forEach((field) => {
+            const id = createNodeId(item[field]);
+
+            if (!(id in nodes)) {
+                nodes[id] = {
+                    id: id,
+                    label: item[field],
+                    internalIp: item[mapper[field]]==='1',
+                    hits: 1
+                };
+            }
+            else {
+                nodes[id].hits++;
+            }
+        });
+    });
+
+    return nodes;
+}
+
+function getLinksFromData(data, nodes) {
+    const links = {};
+
+    data.forEach((item) => {
+        const id = createLinkId(item.srcIP, item.dstIP);
+        const score = -Math.log(item.lda_score);
+
+        if (!(id in links)) {
+            links[id] = {
+                id: id,
+                source: nodes[createNodeId(getLinkSource(item.srcIP, item.dstIP))],
+                target: nodes[createNodeId(getLinkDestination(item.srcIP, item.dstIP))],
+                score
+            };
+        }
+        else {
+            links[id].score  = Math.max(links[id].score, score)
+        }
+    });
+
+    return links;
+}
+
+function getStateFromData({data}) {
+    const nodes = getNodesFromData(data);
+    const links = getLinksFromData(data, nodes);
+
+    return {
+        nodes: Object.keys(nodes).map(nodeId => nodes[nodeId]),
+        links: Object.keys(links).map(linkId => links[linkId])
+    };
+}
+
+const NetworkViewPanel = React.createClass({
+    mixins: [ContentLoaderMixin, ChartMixin, PolloNetworkViewMixin],
+    componentDidMount() {
+      SuspiciousStore.addChangeDataListener(this._onChange);
+      SuspiciousStore.addThreatHighlightListener(this._onHighlight);
+      SuspiciousStore.addThreatUnhighlightListener(this._onUnhighlight);
+      SuspiciousStore.addThreatSelectListener(this._onSelect);
+      window.addEventListener('resize', this.buildChart);
+    },
+    componentWillUmount() {
+      SuspiciousStore.removeChangeDataListener(this._onChange);
+      SuspiciousStore.removeThreatHighlightListener(this._onHighlight);
+      SuspiciousStore.removeThreatUnhighlightListener(this._onUnhighlight);
+      SuspiciousStore.removeThreatSelectListener(this._onSelect);
+      window.removeEventListener('resize', this.buildChart);
+    },
+    // render is inherited from Mixins
+    _onChange() {
+        const data = SuspiciousStore.getData();
+        const state = {loading: data.loading};
+
+        if (state.loading===false && data.data instanceof Array) {
+            state.data = getStateFromData(data);
+            state.data.maxNodes = SpotConstants.MAX_SUSPICIOUS_ROWS;
+        }
+
+        this.setState(state);
+    },
+    _onHighlight() {
+        const threat = SuspiciousStore.getHighlightedThreat();
+
+        this.highlightNodes([createNodeId(threat.srcIP), createNodeId(threat.dstIP)]);
+        this.highlightEdge(createLinkId(threat.srcIP, threat.dstIP));
+    },
+    _onUnhighlight() {
+        this.unhighlight();
+    },
+    _onSelect() {
+        const threat = SuspiciousStore.getSelectedThreat();
+
+        this.selectNodes([createNodeId(threat.srcIP), createNodeId(threat.dstIP)]);
+        this.selectEdge(createLinkId(threat.srcIP, threat.dstIP));
+    },
+    _onClick(id) {
+        const ip = getIpFromNodeId(id);
+        EdInActions.selectIp(ip);
+        SpotActions.toggleMode(SpotConstants.DETAILS_PANEL, SpotConstants.VISUAL_DETAILS_MODE);
+        EdInActions.reloadVisualDetails();
+    },
+    _onContextualClick(id) {
+      d3.event.preventDefault();
+
+      const ip = getIpFromNodeId(id);
+      EdInActions.setFilter(ip);
+      EdInActions.reloadSuspicious();
+    }
+});
+
+module.exports = NetworkViewPanel;
---
+++ spot-oa/ui/js/components/PolloNetworkViewMixin.react.js
@@ -0,0 +1,242 @@
+const Base64 = require('js-base64').Base64;
+const d3Interpolate = require('d3-interpolate');
+
+const ID_REPLACE = '=';
+const ID_REPLACEMENT = '_';
+
+function encodeId(id) {
+    return Base64.encode(id).replace(new RegExp(`[${ID_REPLACE}]`, 'g'), ID_REPLACEMENT);
+}
+
+function decodeId(id) {
+    return Base64.decode(id.replace(new RegExp(`[${ID_REPLACEMENT}]`, 'g'), ID_REPLACE));
+}
+
+function getRadious(area) {
+    return Math.sqrt(area/Math.PI);
+}
+
+const PolloNetworkViewMixin = {
+    buildChart() {
+        const svgSel = d3.select(this.svg);
+
+        // Enable zoom behaviour
+        this.zoom = d3.behavior.zoom().on("zoom", this.onZoom);
+        this.canvas = svgSel.append('g');
+        svgSel.call(this.zoom);
+
+        // Node related scales
+        const hitsDomain = [1, this.state.data.maxNodes];
+        const ipDomain = [false, true];
+        this.nodeSizeScale = d3.scale.linear().domain(hitsDomain);
+        this.chargeScale = d3.scale.linear().domain(hitsDomain);
+        this.typeScale = d3.scale.ordinal().domain(ipDomain).range(["circle", "diamond"]);
+        this.colorScale = d3.scale.ordinal().domain(ipDomain).range(["#fdb813", "#0071C5"]);
+
+        // Link related scales
+        this.opacityScale = d3.scale.threshold().domain([13]).range([0.1, 1]);
+        this.linkColorScale = d3.scale.linear()
+                                      .domain([16, 13, 12, 2])
+                                      .range([d3.hsl(214, 0.04, 0.34), d3.hsl(216, 0.02, 0.59), d3.hsl(216, 0.69, 0.84), d3.hsl(201, 0.1, 0.72)])
+                                      .interpolate(d3Interpolate.interpolateCubehelix);
+
+        // Create a new force layout
+        this.force = d3.layout.force();
+
+        // Add and update nodes and links on every tick event from the force layout
+        this.force.on('tick', () => {
+            this.drawLinks();
+            this.drawNodes();
+        });
+
+        // Create a tooltip
+        this.tooltip = d3.tip()
+            .attr('class', 'd3-tip')
+            .html(n => `${n.label}<br/> <small class="text-muted">Right click to apply this node as filter</small>`);
+
+        this.canvas.call(this.tooltip);
+    },
+    onZoom: function () {
+        const translate = d3.event.translate;
+        const scale = d3.event.scale;
+
+        this.canvas.attr('transform', `translate(${translate})scale(${scale})`);
+    },
+    draw() {
+        const svgElement = $(this.svg);
+
+        svgElement.width('100%').height('100%');
+
+        const size = [svgElement.width(), svgElement.height()];
+        /*
+         * Calculate the max area a node can have.
+         *
+         * 1. Get the smaller value betwenn width and height of the canvas.
+         * 2. Calculate the area of imaginary square whose edge length equals the previous value.
+         * 3. Set maxNodeArea to the 40% of the available area of our imaginary square
+         */
+        const maxNodeArea = Math.pow(Math.min(size[0], size[1]), 2) * 0.1;
+
+        // The biggest node we can have goes up to the 50% of maxNodeArea
+        this.nodeSizeScale.range([maxNodeArea*0.01, maxNodeArea*0.5]);
+        const sizeDomain = this.nodeSizeScale.domain();
+        this.chargeScale.range([-this.nodeSizeScale(sizeDomain[0]), -this.nodeSizeScale(sizeDomain[1])])
+
+        this.force
+            .stop()
+            .size(size)
+            .charge(-1000)
+            .linkDistance(l => {
+                return [l.source, l.target].reduce((length, n) => {
+                    return length + getRadious(this.nodeSizeScale(n.hits));
+                }, 50);
+            })
+            .chargeDistance(getRadious(this.nodeSizeScale(this.nodeSizeScale.domain()[1])) * 4)
+            .nodes(this.state.data.nodes)
+            .links(this.state.data.links);
+
+        this.force.start();
+
+        setTimeout(() => {
+            this.force.stop()
+                .charge(n => {
+                    return this.chargeScale(n.weight);
+                })
+                .start();
+        }, 1000);
+
+        // Tooltip margins
+        this.tooltip.box = [size[1]*.4, size[0]*.8, size[1]*.8, size[0]*.4];
+    },
+    drawNodes(nodes) {
+        this.nodesSel = {
+            update: this.canvas.selectAll('.node').data(this.state.data.nodes, n => encodeId(n.id))
+        };
+        this.nodesSel.enter = this.nodesSel.update.enter();
+
+        // Add new nodes
+        this.nodesSel.enter
+            .append('path', '.edge')
+            .classed('node', true)
+            .attr("id", n => encodeId(n.id))
+            .attr("d", d3.svg.symbol()
+               .size(n => this.nodeSizeScale(n.hits))
+               .type(n => this.typeScale(n.internalIp))
+            )
+            .attr('fill', n => this.colorScale(n.internalIp))
+            .call(this.force.drag)
+            .on('mouseover', (n) => {
+                var direction = '';
+
+                // Decide where should the tooltip be displayed?
+
+                // Vertically
+                if (d3.event.layerY<this.tooltip.box[0]) {
+                    direction = 's';
+                }
+                else if (d3.event.layerY>this.tooltip.box[2]) {
+                    direction = 'n';
+                }
+
+                // Horizontally
+                if (d3.event.layerX>this.tooltip.box[1]) {
+                    direction += 'w';
+                }
+                else if (d3.event.layerX<this.tooltip.box[3]) {
+                    direction += 'e'
+                }
+
+                direction = direction || 'n';
+
+                this.tooltip.direction(direction);
+                this.tooltip.show.call(this, n);
+            })
+            .on("mousedown", n => {
+                d3.event.stopPropagation();
+            })
+            .on("click", n => {
+                this._onClick(n.id);
+            })
+            .on("contextmenu", n => {
+                this.tooltip.hide();
+
+                this._onContextualClick(n.id);
+            })
+            .on('mouseout', () => {
+                this.tooltip.hide();
+            });
+
+        this.nodesSel.update
+            .attr('transform', n => `translate(${n.x},${n.y})`)
+            .transition()
+                .attr("d", d3.svg.symbol()
+                   .size(n => this.nodeSizeScale(n.hits))
+                   .type(n => this.typeScale(n.internalIp))
+                );
+    },
+    drawLinks(links) {
+        this.linksSel = {
+            update: this.canvas.selectAll('.edge').data(this.state.data.links, l => encodeId(l.id))
+        };
+        this.linksSel.enter = this.linksSel.update.enter();
+
+        // Add new links
+        this.linksSel.enter
+             .append('line')
+             .classed('edge', true)
+             .attr("id", l => encodeId(l.id))
+             .style('stroke', l => this.linkColorScale(l.score))
+             .style('stroke-opacity', l => this.opacityScale(l.score));
+
+        // Update links
+        this.linksSel.update
+            .attr("x1", l => l.source.x)
+            .attr("y1", l => l.source.y)
+            .attr("x2", l => l.target.x)
+            .attr("y2", l => l.target.y);
+    },
+    highlightNodes(ids) {
+        ids = ids instanceof Array ? ids : [ids];
+
+        this.canvas.selectAll('.node').classed('node-faded', true);
+
+        ids.forEach(id => {
+            id = encodeId(id);
+            this.canvas.select(`#${id}.node`).classed("node-faded", false);
+        })
+    },
+    highlightEdge(id) {
+        id = encodeId(id);
+        this.canvas.selectAll('.edge').classed('edge-faded', true);
+
+        this.canvas.select(`#${id}.edge`)
+            .classed('edge-faded', false)
+            .classed('highlight', true);
+    },
+    unhighlight() {
+        this.canvas.selectAll('.node').classed('node-faded', false);
+        this.canvas.selectAll('.edge')
+            .classed('edge-faded', false)
+            .classed('highlight', false);
+    },
+    selectNodes(ids) {
+        ids = ids instanceof Array ? ids : [ids];
+
+        this.canvas.selectAll('.node').classed('blink_me', false);
+
+        ids.forEach((id) => {
+            id = encodeId(id);
+            this.canvas.select(`#${id}.node`).classed('blink_me', true);
+        });
+    },
+    selectEdge(id) {
+        id = encodeId(id);
+        this.canvas.selectAll('.edge')
+            .classed('blink_me', false)
+            .filter(`#${id}`)
+                .classed('active', true)
+                .classed('blink_me', true);
+    }
+};
+
+module.exports = PolloNetworkViewMixin;
