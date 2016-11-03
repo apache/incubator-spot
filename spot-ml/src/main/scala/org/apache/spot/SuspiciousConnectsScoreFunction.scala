a9e46ffe63e7652fe244533758abf27c3a1e3c8c
Brandon Edwards <brandon.edwards@intel.com>
2016-11-03 12:58:47 -0700

    Inserted comment noting that a missing ip or word key means not seen in training.

--- spot-ml/src/main/scala/org/apache/spot/SuspiciousConnectsScoreFunction.scala
+++ spot-ml/src/main/scala/org/apache/spot/SuspiciousConnectsScoreFunction.scala
@@ -12,6 +12,7 @@ class SuspiciousConnectsScoreFunction(topicCount: Int,
 
     val zeroProb = Array.fill(topicCount) { 0d }
 
+    // If either the ip or the word key value cannot be found it means that it was not seen in training.
     val topicGivenDocProbs = ipToTopicMixBC.value.getOrElse(ip, zeroProb)
     val wordGivenTopicProbs = wordToPerTopicProbBC.value.getOrElse(word, zeroProb)
 
