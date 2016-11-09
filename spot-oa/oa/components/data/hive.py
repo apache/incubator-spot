692ff6673e4986462c3f572340aaa78f8281e9b8
LedaLima <duxbury@gmsonide01.zpn.intel.com>
2016-11-09 15:31:27 -0600

    Fixed bug in output column delimitter

--- spot-oa/oa/components/data/hive.py
+++ spot-oa/oa/components/data/hive.py
@@ -7,11 +7,13 @@ class Engine(object):
         self._pipeline = pipeline
 
     def query(self,query,output_file=None, delimiter=','):
-        hive_config = "set mapred.max.split.size=1073741824;set hive.exec.reducers.max=10;set hive.cli.print.header=true; "
-
-        if output_file:
-            hive_cmd = "hive -S -e \"{0} {1}\" | sed 's/[\t]/,/g' | sed '/INFO\|WARNING\|DEBUG/d' > {2}".format(hive_config,query,output_file)
-        else:
-            hive_cmd = "hive -S -e \"{0} {1}\"".format(hive_config,delimiter,query)
+        hive_config = "set mapred.max.split.size=1073741824;set hive.exec.reducers.max=10;set hive.cli.print.header=true;"
         
+        del_format = "| sed 's/[\t]/{0}/g'".format(delimiter)
+        if output_file: 
+            hive_cmd = "hive -S -e \"{0} {1}\" {2} | sed '/INFO\|WARNING\|DEBUG/d' > {3}".format(hive_config,query,del_format,output_file)
+        else:
+            hive_cmd = "hive -S -e \"{0} {1}\"".format(hive_config,query)
+        
+        print hive_cmd
         check_output(hive_cmd,shell=True)
