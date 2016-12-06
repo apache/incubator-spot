f38c397d21123f17d1920821f76ffe54f2d346c8
Diego Ortiz Huerta <diego.ortiz.huerta@intel.com>
2016-12-06 08:53:11 -0800

    Ingest summary is now a top level section

--- spot-oa/ui/js/ingest-summary.js
+++ spot-oa/ui/js/ingest-summary.js
@@ -3,14 +3,12 @@ const ReactDOM = require('react-dom');
 
 const SpotActions = require('./actions/SpotActions');
 const InSumActions = require('./actions/InSumActions');
-const IngestSummaryStore = require('./stores/IngestSummaryStore');
 const SpotConstants = require('./constants/SpotConstants');
 const SpotUtils = require('./utils/SpotUtils');
 const DateUtils = require('./utils/DateUtils');
 
 // Build and Render Toolbar
 const DateInput = require('./components/DateInput.react');
-const OptionPicker = require('./components/OptionPicker.react');
 
 // Find out period
 var startDate, endDate, today;
@@ -43,48 +41,32 @@ if (endDate < startDate)
   endDate = today;
 }
 
-const PIPELINES = IngestSummaryStore.PIPELINES;
-const DEFAULT_PIPELINE = Object.keys(PIPELINES)[0];
-
-const loadPipeline = function loadPipeline(pipeline) {
-    IngestSummaryStore.setPipeline(pipeline);
-    InSumActions.reloadSummary();
-}
-
 ReactDOM.render(
+  (
     <form className="form-inline">
-        <div className="form-group">
-            <label htmlFor="pipeline-picker">Source: </label>
-            <div className="input-group input-group-xs">
-                <OptionPicker
-                    id="pipeline-picker"
-                    options={PIPELINES}
-                    value={DEFAULT_PIPELINE}
-                    onChange={loadPipeline} />
-            </div>
+      <div className="form-group">
+        <label htmlFor="startDatePicker">Period:</label>
+        <div className="input-group input-group-xs">
+          <div className="input-group-addon">
+            <span className="glyphicon glyphicon-calendar" aria-hidden="true"></span>
+          </div>
+          <DateInput id="startDatePicker" name={SpotConstants.START_DATE} value={startDate}/>
         </div>
-        <div className="form-group">
-            <label htmlFor="startDatePicker">Period:</label>
-            <div className="input-group input-group-xs">
-                <div className="input-group-addon">
-                    <span className="glyphicon glyphicon-calendar" aria-hidden="true"></span>
-                </div>
-                <DateInput id="startDatePicker" name={SpotConstants.START_DATE} value={startDate}/>
-            </div>
+      </div>
+      <div className="form-group">
+        <label htmlFor="endDatePicker"> - </label>
+        <div className="input-group input-group-xs">
+          <DateInput id="endDatePicker" name={SpotConstants.END_DATE} value={endDate} />
+          <div className="input-group-btn">
+            <button className="btn btn-default" type="button" title="Reload" onClick={InSumActions.reloadSummary}>
+              <span className="glyphicon glyphicon-repeat" aria-hidden="true"></span>
+            </button>
+          </div>
         </div>
-        <div className="form-group">
-            <label htmlFor="endDatePicker"> - </label>
-            <div className="input-group input-group-xs">
-                <DateInput id="endDatePicker" name={SpotConstants.END_DATE} value={endDate} />
-                <div className="input-group-btn">
-                    <button className="btn btn-default" type="button" title="Reload" onClick={InSumActions.reloadSummary}>
-                        <span className="glyphicon glyphicon-repeat" aria-hidden="true"></span>
-                    </button>
-                </div>
-            </div>
-        </div>
-    </form>,
-    document.getElementById('nav_form')
+      </div>
+    </form>
+  ),
+  document.getElementById('nav_form')
 );
 
 // Build and Render Edge Investigation's panels
@@ -97,7 +79,7 @@ ReactDOM.render(
   <div id="spot-content">
     <PanelRow maximized>
       <Panel title="Ingest Summary" container header={false} className="col-md-12">
-        <IngestSummaryPanel className="is-chart" />
+        <IngestSummaryPanel id="spot-is" />
       </Panel>
     </PanelRow>
   </div>,
@@ -109,4 +91,4 @@ SpotActions.setDate(startDate, SpotConstants.START_DATE);
 SpotActions.setDate(endDate, SpotConstants.END_DATE);
 
 // Load data
-loadPipeline(DEFAULT_PIPELINE);
+InSumActions.reloadSummary();
