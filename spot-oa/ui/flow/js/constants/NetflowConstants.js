46470640ef882297df1109d4f3484f017cf63d95
Diego Ortiz Huerta <diego.ortiz.huerta@intel.com>
2016-12-12 10:08:56 -0800

    Ingest summary supporting 3 use cases, Netflow, DNS and Proxy

--- spot-oa/ui/flow/js/constants/NetflowConstants.js
+++ spot-oa/ui/flow/js/constants/NetflowConstants.js
@@ -5,7 +5,6 @@ var NetflowConstants = {
   API_VISUAL_DETAILS: '../../data/flow/${date}/chord-${ip}.tsv',
   API_COMMENTS: '../../data/flow/${date}/threats.csv',
   API_INCIDENT_PROGRESSION: '../../data/flow/${date}/threat-dendro-${ip}.json',
-  API_INGEST_SUMMARY: '../data/flow/ingest_summary/is_${year}${month}.csv',
   API_IMPACT_ANALYSIS: '../../data/flow/${date}/stats-${ip}.json',
   API_GLOBE_VIEW: '../../data/flow/${date}/globe-${ip}.json',
   API_WORLD_110M: '../flow/world-110m.json',
