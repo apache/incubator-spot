9db16458d677800a558aef4b3f3f68238600acc8
natedogs911 <natedogs911@gmail.com>
2017-02-23 12:35:53 -0800

    install.sh for root installs, build.sh for /opt/spot/bin installs

--- spot-ingest/build.sh
+++ spot-ingest/build.sh
@@ -5,7 +5,6 @@ wshark_vers=2.2.3
 local_path=`pwd`
 source_path=/tmp/ingest_src
 install_path=/opt/spot/
-dependencies=(tar wget screen python)
 missing_dep=()
 wget_cmd="wget -nc --no-check-certificate"
 untar_cmd="tar -xvf"
@@ -15,17 +14,9 @@ mk_opt="-j `nproc`"
 # functions
 
 log_cmd () {
-    printf "\n****SPOT.INGEST.Install.sh****\n"
+    printf "\n****SPOT.INGEST.build.sh****\n"
     date +"%y-%m-%d %H:%M:%S"
     printf "$1\n\n"
-}
-
-check_root () {
-    # checking for root as many of these functions interact with system owned directories
-    if [[ "$EUID" -ne 0 ]]; then
-            log_cmd "Non root user detected, Please run as root or with sudo"
-            exit 1
-    fi
 }
 
 cleanup () {
@@ -33,43 +24,7 @@ cleanup () {
     rm -rf ${source_path}
 }
 
-check_bin () {
-	# check_bin can be used to verify if a certain binary is already installed
-    for item in "$@"; do 
-     	if type ${item} >/dev/null 2>&1; then
-            log_cmd "${item} found"
-        else
-            missing_dep+=(${item})
-        fi
-    done
-}
-
-install_pkg () {
-	# if no parameters this will simply install any $missing_deps
-	# if any parameters provided they will be added to $missing_dep
-    if [[ "$@" ]]; then
-        for item in "$@"; do
-	    missing_dep+=(${item})
-        done
-    fi
-
-    if [[ "${missing_dep[@]}" ]]; then
-        log_cmd "installing ${missing_dep[@]}"
-        ${install_cmd} ${missing_dep[@]}
-	unset missing_dep[*]
-    fi
-}
-
 install_tshark () {
-    if [ "${host_os}" == "debian" ]; then
-        log_cmd "installing dependencies for tshark installation"
-        check_bin make bzip2 pkg-config libsmi flex bison byacc
-        install_pkg libpcap-dev heimdal-dev libc-ares-dev 
-    elif [ "${host_os}" == "rhel" ]; then
-        check_bin make bzip2 gcc bison
-        install_pkg glib2-devel flex-devel libsmi-devel libpcap-devel
-    fi
-
     ${wget_cmd} https://1.na.dl.wireshark.org/src/wireshark-${wshark_vers}.tar.bz2 -P ${source_path}/
     ${untar_cmd} ${source_path}/wireshark-${wshark_vers}.tar.bz2 -C ${source_path}/
     cd ${source_path}/wireshark-${wshark_vers}
@@ -91,23 +46,24 @@ install_nfdump () {
     source ./install_nfdump.sh ${install_path}
     cd ${local_path}
 }
+
+check_os () {
+        # detect distribution
+        # to add other distributions simply create a test case with installation commands
+        if [ -f /etc/redhat-release ]; then
+                install_cmd="yum -y install"
+                log_cmd "installation command: $install_cmd"
+                host_os="rhel"
+        elif [ -f /etc/debian_version ]; then
+                install_cmd="apt-get install -yq"
+                log_cmd "installation command: $install_cmd"
+                host_os="debian"
+                apt-get update
+        fi
+}
 # end functions
 
-check_root
-
-# detect distribution
-# to add other distributions simply create a test case with installation commands
-
-if [ -f /etc/redhat-release ]; then
-    install_cmd="yum -y install"
-    log_cmd "installation command: $install_cmd"
-    host_os="rhel"
-elif [ -f /etc/debian_version ]; then
-    install_cmd="apt-get install -yq"
-    log_cmd "installation command: $install_cmd"
-    host_os="debian"
-    apt-get update
-fi
+check_os
 
 if [ ! -d ${source_path} ]; then
         mkdir ${source_path}
@@ -117,10 +73,6 @@ if [ ! -d ${install_path} ]; then
         log_cmd "${install_path} not created, Please run spot-setup/local_setup.sh first"
         exit 1        
 fi
-
-# check basic dependencies
-check_bin ${dependencies[@]}
-install_pkg
 
 if type tshark >/dev/null 2>&1; then
         log_cmd "tshark found"
@@ -134,15 +86,6 @@ if type nfdump >/dev/null 2>&1; then
 else
     log_cmd "missing nfdump"
     install_nfdump
-fi
-
-if type pip >/dev/null 2>&1; then
-    log_cmd "pip found"
-else
-    log_cmd "missing pip"
-    ${wget_cmd} https://bootstrap.pypa.io/get-pip.py -P ${source_path}/
-    python ${source_path}/get-pip.py
-    log_cmd "pip installed"
 fi
 
 if [ -z ${local_path}/requirements.txt ]; then
