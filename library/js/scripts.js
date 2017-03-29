3d7ac91ff865cd817183695944facd335796556c
Cesar Berho <cesar@apache.org>
2017-03-28 18:47:02 -0600

    New menu on Project website, for Coomunity, includes links to mailing lists, Slack, social media, and Committers directory

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -93,16 +93,7 @@ var timeToWaitForLast = 100;
  * images on mobile to save bandwidth. Once we hit an acceptable viewport
  * then we can swap out those images since they are located in a data attribute.
 */
-function loadGravatars() {
-  // set the viewport using the function above
-  viewport = updateViewportDimensions();
-  // if the viewport is tablet or larger, we load in the gravatars
-  if (viewport.width >= 768) {
-  jQuery('.comment img[data-gravatar]').each(function(){
-    jQuery(this).attr('src',jQuery(this).attr('data-gravatar'));
-  });
-	}
-} // end function
+
 
 
 /*
