49c32a4cb1fa5c7176435283d13a6c1f57538388
Austin Leahy <leahy@apache.org>
2021-09-20 22:54:09 -0600

    missed object from previous commit

---
+++ spot-operational-data-model/package/objects/src/tls.ts
@@ -0,0 +1,13 @@
+export interface Tls {
+    // tls_version	string	Coming soon	Coming soon
+    // tls_cipher	string	Coming soon	Coming soon
+    // tls_curve	string	Coming soon	Coming soon
+    // tls_server_name	string	Coming soon	Coming soon
+    // tls_resumed	boolean	Coming soon	Coming soon
+    // tls_next_protocol	string	Coming soon	Coming soon
+    // tls_established	boolean	Coming soon	Coming soon
+    // tls_cert_chain_fuids	string	Coming soon	Coming soon
+    // tls_client_cert_chain_fuids	string	Coming soon	Coming soon
+    // tls_subject	string	Coming soon	Coming soon
+    // tls_issuer	string	Coming soon	Coming soon
+}
